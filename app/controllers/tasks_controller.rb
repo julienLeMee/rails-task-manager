943c549c139a3bd6d909617712b2d583c027cb3e
Julien Le Mee <julemee@gmail.com>
2022-08-09 14:55:55 -0400

    rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,13 +14,23 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to task_path(@task)
+    redirect_to tasks_path(@task)
   end
 
   def edit
     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to task_path(@task)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@task), status: :see_other
   end
 
   private
